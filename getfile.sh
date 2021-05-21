59842b1beca0b228731560f0fe42941e21048926
Rui Santos <rui.santos@vodafone.com>
2021-05-21 13:33:02 +0100

    code uploaded

---
+++ getfile.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+set -e
+
+role=$(curl -s "http://169.254.169.254/latest/meta-data/iam/security-credentials/")
+role_credentials=$(curl -s "http://169.254.169.254/latest/meta-data/iam/security-credentials/${role}")
+AWS_ACCESS_KEY_ID=$(echo "${role_credentials}" | cut -d, -f 3 | tr -d \"| awk -F: '{ print $2 }')
+AWS_SECRET_ACCESS_KEY=$(echo "${role_credentials}" | cut -d, -f 4 | tr -d \"| awk -F: '{ print $2 }')
+AWS_SECURITY_TOKEN=$(echo "${role_credentials}" | cut -d, -f 5 | tr -d \"| awk -F: '{ print $2 }')
+# functions
+function get_hmac_sha256 {
+  if [ $# -le 1 ]; then
+    echo "error: get_hmac_sha256 needs args"
+    echo "usage:"
+    echo "  get_hmac_sha256 key:<key> <data>"
+    exit 1
+  fi
+
+  key="$1"
+  data="$2"
+
+  echo -n "$data" |  openssl dgst -sha256 -mac HMAC -macopt "$key" | awk '{print $2}'
+}
+
+
+usage="Usage: $0 bucket:dir/file (e.g $0 bucket:destination/folder/file.txt)"
+Key=${1:?"Error: ${usage}"}
+
+s3Bucket=$(echo ${Key} | cut -d: -f 1)
+#s3Bucket="iotbigdatarawfilesid-lit"
+object=$(echo ${Key} | cut -d: -f 2)
+#destinationKey="outputfiles/MZ/file"
+fileName=$(basename ${object})
+
+# s3 account info
+region="eu-central-1"
+endpoint="${s3Bucket}.s3.${region}.amazonaws.com"
+service="s3"
+
+
+# date, timestamp, hashes, etc.
+yyyymmdd=$(date +%Y%m%d)
+date=$(date -u +%Y%m%dT%H%M%SZ)
+empty_hash=$(openssl dgst -sha256 /dev/null | awk '{print $2}')
+http_req="GET"
+
+
+echo "Downloading ${fileName} to s3://${s3Bucket}/${object} in ${region}"
+
+# build canonical request
+cr_str="${http_req}
+/${object}
+
+host:$endpoint
+x-amz-content-sha256:$empty_hash
+x-amz-date:${date}
+x-amz-security-token:${AWS_SECURITY_TOKEN}
+
+host;x-amz-content-sha256;x-amz-date;x-amz-security-token
+${empty_hash}"
+
+cr_hash=`echo -n "${cr_str}" | openssl dgst -sha256 | awk '{print $2}'`
+
+# build string to sign
+sts="AWS4-HMAC-SHA256
+${date}
+${yyyymmdd}/${region}/${service}/aws4_request
+$cr_hash"
+
+# signing key from date keys
+DateKey=$(get_hmac_sha256 key:"AWS4${AWS_SECRET_ACCESS_KEY}" ${yyyymmdd})
+DateRegionKey=$(get_hmac_sha256 hexkey:${DateKey} ${region})
+DateRegionServiceKey=$(get_hmac_sha256 hexkey:${DateRegionKey} ${service})
+SigningKey=$(get_hmac_sha256 hexkey:${DateRegionServiceKey} "aws4_request")
+req_signature=$(get_hmac_sha256 hexkey:${SigningKey} "${sts}")
+
+auth_hdr="Authorization: AWS4-HMAC-SHA256 Credential=${AWS_ACCESS_KEY_ID}/${yyyymmdd}/${region}/s3/aws4_request, \
+          SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token, \
+          Signature=${req_signature}"
+
+curl -s \
+  -o ${fileName} \
+  -H "${auth_hdr}" \
+  -H "x-amz-content-sha256: ${empty_hash}" \
+  -H "x-amz-date: ${date}" \
+  -H "x-amz-security-token: ${AWS_SECURITY_TOKEN}" \
+  "https://${endpoint}/${object}"
---
+++ putfile.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+set -e
+
+role=$(curl -s "http://169.254.169.254/latest/meta-data/iam/security-credentials/")
+role_credentials=$(curl -s "http://169.254.169.254/latest/meta-data/iam/security-credentials/${role}")
+AWS_ACCESS_KEY_ID=$(echo "${role_credentials}" | cut -d, -f 3 | tr -d \"| awk -F: '{ print $2 }')
+AWS_SECRET_ACCESS_KEY=$(echo "${role_credentials}" | cut -d, -f 4 | tr -d \"| awk -F: '{ print $2 }')
+AWS_SECURITY_TOKEN=$(echo "${role_credentials}" | cut -d, -f 5 | tr -d \"| awk -F: '{ print $2 }')
+
+usage="Usage: $0 file bucket:dir (e.g $0 file.txt bucket:destination/folder/)"
+fileName=${1:?"Error: ${usage}"}
+destination=${2:?"Error: ${usage}"}
+
+s3Bucket=$(echo $destination | cut -d: -f 1)
+#s3Bucket="iotbigdatarawfilesid-lit"
+destinatonKey=$(echo $destination | cut -d: -f 2)
+#detinationKey="outputfiles/MZ/"
+
+yyyymmdd=$(date +%Y%m%d)
+region="eu-central-1"
+endpoint="${s3Bucket}.s3.${region}.amazonaws.com"
+contentLength=$(cat ${fileName} | wc -c)
+contentHash=$(openssl sha -sha256 -hex ${fileName} | sed 's/.* //')
+b64=$(openssl md5 -binary "$fileName" | openssl base64)
+acl="bucket-owner-full-control"
+date=$(date -u +%Y%m%dT%H%M%SZ)
+expdate_s=$(date --date '1 month' -u +%Y-%m-%dT%H:%M:%SZ)
+
+echo "Uploading ${fileName} to s3://${s3Bucket}/${destinatonKey}${fileName} in ${region}"
+
+policy=$(cat <<POLICY | openssl base64 | tr -d \\n
+{ "expiration": "${expdate_s}T12:00:00.000Z",
+  "conditions": [
+    {"acl": "$acl" },
+    {"bucket": "$s3Bucket" },
+    ["starts-with", "\$key", ""],
+    {"x-amz-date": "$date" },
+    {"content-md5": "$b64" },
+    {"x-amz-credential": "${AWS_ACCESS_KEY_ID}/${yyyymmdd}/${region}/s3/aws4_request" },
+    {"x-amz-security-token": "${AWS_SECURITY_TOKEN}" },
+    {"x-amz-algorithm": "AWS4-HMAC-SHA256" }
+  ]
+}
+POLICY
+)
+
+# calculate the signing key
+DateKey=$(echo -n "${yyyymmdd}" | openssl sha -sha256 -hex -hmac "AWS4${AWS_SECRET_ACCESS_KEY}" | sed 's/.* //')
+DateRegionKey=$(echo -n "${region}" | openssl sha -sha256 -hex -mac HMAC -macopt hexkey:${DateKey} | sed 's/.* //')
+DateRegionServiceKey=$(echo -n "s3" | openssl sha -sha256 -hex -mac HMAC -macopt hexkey:${DateRegionKey} | sed 's/.* //')
+SigningKey=$(echo -n "aws4_request" | openssl sha -sha256 -hex -mac HMAC -macopt hexkey:${DateRegionServiceKey} | sed 's/.* //')
+# then, once more a HMAC for the signature
+signature=$(echo -en ${policy} | openssl sha -sha256 -hex -mac HMAC -macopt hexkey:${SigningKey} | sed 's/.* //')
+
+key_and_sig_args="-F X-Amz-Credential=${AWS_ACCESS_KEY_ID}/${yyyymmdd}/${region}/s3/aws4_request -F X-Amz-Algorithm=AWS4-HMAC-SHA256 -F X-Amz-Signature=$signature -F X-Amz-Date=${date}"
+
+curl -s  \
+-F "key=${destinatonKey}${fileName}" \
+-F "acl=$acl" \
+-F "X-Amz-Credential=${AWS_ACCESS_KEY_ID}/${yyyymmdd}/${region}/s3/aws4_request" \
+-F "X-Amz-Algorithm=AWS4-HMAC-SHA256" \
+-F "X-Amz-Signature=${signature}" \
+-F "X-Amz-Date=${date}" \
+-F "content-md5=${b64}" \
+-F "Policy=$policy" \
+-F "X-Amz-Security-Token=${AWS_SECURITY_TOKEN}" \
+-F "file=@$fileName" \
+https://${endpoint}/
